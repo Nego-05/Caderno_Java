e54872bd0037418df7c4fcca90182e457ebd4816
Nego-05 <125407854+Nego-05@users.noreply.github.com>
2023-05-26 15:52:06 -0300

    Add files via upload

---
+++ main.kt
@@ -0,0 +1,20 @@
+fun main() {
+  println("Olá Mundo")
+  var nome: String = "Pedro"
+  val nascimento: Int = 2006
+
+  println(nome)          // exibe o nome
+  println(nascimento)     // exibe o nascimento
+
+  val myNum: Int = 8                // Int
+  val myDoubleNum: Double = 8.99    // Double
+  val myLetter: Char = 'P'          // Char
+  val myBoolean: Boolean = false     // Boolean
+  val myText: String = "Opa"      // String
+
+  println (myNum)
+  println (myDoubleNum)
+  println (myLetter)
+  println (myBoolean)
+  println (myText)
+}
