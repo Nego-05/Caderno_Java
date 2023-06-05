60a4ce1c4b469d1c04f166539b0dc66b8bdc16cf
Nego-05 <125407854+Nego-05@users.noreply.github.com>
2023-06-05 14:55:37 -0300

    Add files via upload

---
+++ MetodosJava.java
@@ -0,0 +1,36 @@
+class Main {
+   static void impPrime(){
+    System.out.println("Só imprime isso");
+  }
+  
+  static void meuMetodo(String nome) {
+    System.out.println("O nome é: " + nome);
+  }
+
+
+    static void nomeEidade(String nome,int ano) {
+    System.out.println(nome + " tem "+ano + " anos");
+  }
+
+    
+  public static void main (String[] args){
+
+  impPrime();
+  
+  meuMetodo("Pedro");
+  meuMetodo("Pedro");
+  meuMetodo("Pedro");
+
+  nomeEidade("Pedro", 17);
+  nomeEidade("Pedro", 17);
+  nomeEidade("Pedro", 17);
+
+                           
+  
+}
+
+
+
+
+  
+}
