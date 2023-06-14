54140397933997857f46914f3e42a560d4b6d9cc
Nego-05 <125407854+Nego-05@users.noreply.github.com>
2023-06-14 16:11:22 -0300

    Add files via upload

--- inputk.kt
+++ inputk.kt
@@ -20,33 +20,33 @@ fun main(args: Array<String>) {
   println("Digite um número: ")
   var nume1 = readLine()!!
     var a = nume1.toInt()
-    println("O número é: " +num1)
+    println("O número é: " +nume1)
   println("Digite outro número: ")
   var nume2 = readLine()!!
   var b = nume2.toInt()
-  println("O número é: " +num2)
+  println("O número é: " +nume2)
   var subtração = a-b
   println("O resultado é: " +subtração)
 
   println("Digite um número: ")
   var numer1 = readLine()!!
     var c = numer1.toInt()
-    println("O número é: " +num1)
+    println("O número é: " +numer1)
   println("Digite outro número: ")
   var numer2 = readLine()!!
     var d = numer2.toInt()
-  println("O número é: " +num2)
+  println("O número é: " +numer2)
   var multiplicação = c*d
   println("O resultado é: " +multiplicação)
 
   println("Digite um número: ")
   var numero1 = readLine()!!
     var e = numero1.toInt()
-    println("O número é: " +num1)
+    println("O número é: " +numero1)
   println("Digite outro número: ")
   var numero2 = readLine()!!
   var f = numero2.toInt()
-  println("O número é: " +num2)
+  println("O número é: " +numero2)
   var divisão = e/f
   println("O resultado é: " +divisão)
 }
